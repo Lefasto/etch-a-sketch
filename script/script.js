bce9ff5dd5ed884b8d4a9d63a7fee90044e4dfac
Gabriel Palfoeldi <g.palfoeldi@gmail.com>
2026-06-09 15:50:54 +0200

    grid creation

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector(".container");
+
+const size = 16;
+
+function createGrid() {
+  for (let i = 0; i < size; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    container.appendChild(column);
+    for (let j = 0; j < size; j++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      column.appendChild(row);
+    }
+  }
+}
+
+createGrid();
