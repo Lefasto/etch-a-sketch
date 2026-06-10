92f8d65f52dfcd49ece1a80bd4b5a7eab79b8443
Gabriel Palfoeldi <g.palfoeldi@gmail.com>
2026-06-10 09:59:11 +0200

    added button to refresh the grid

--- script/script.js
+++ script/script.js
@@ -1,26 +1,37 @@
 const container = document.querySelector(".container");
+const setBtn = document.querySelector(".set-btn");
 
 const size = 16;
 
+let rowSize = 0;
+let columnSize = 0;
+
+container.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("column")) {
+    e.target.style.background = "greenyellow";
+  }
+});
+
+function removeChildElements(container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
 function createGrid() {
   for (let i = 0; i < size; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    container.appendChild(column);
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
     for (let j = 0; j < size; j++) {
-      const row = document.createElement("div");
-      row.classList.add("row");
-      column.appendChild(row);
+      const column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
     }
   }
 }
 
-createGrid();
-
-const hoveredState = document.querySelectorAll(".row");
-
-hoveredState.forEach((row) => {
-  row.addEventListener("mouseover", (e) => {
-    e.target.style.background = "greenyellow";
-  });
+setBtn.addEventListener("click", () => {
+  removeChildElements(container);
+  createGrid();
 });
